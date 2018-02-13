684d484af94f8a95c3b2834286f54a957cb02f2c
Gilmar Vanin <jvanin@thoughtworks.com>
2018-02-13 18:05:10 -0300

    Fixes bugs that with redundant form tag used twice

--- src/itemList/components/itemList.js
+++ src/itemList/components/itemList.js
@@ -9,25 +9,23 @@ const ItemList = (state) => {
   const items = state.items.searchedItems.length !== 0 ? state.items.searchedItems : state.items.availables
 
   return (
-    <form>
-      <ul className="shopping-card">
-      {
-        items.map(item =>
-          (
-            <form onSubmit={ handleSubmit(() => state.addToCart(item.id)) }>
-              <li key={ item.id }>
-                <p className="price">${ item.price }</p>
-                <span>{ item.title }</span>
-                <img src="./shopping-cart.png" alt="ilustrative item" />
-                <p> { item.description }</p>
-                <button type="submit">Add to cart</button>
-              </li>
-            </form>
-          )
+    <ul className="shopping-card">
+    {
+      items.map(item =>
+        (
+          <form key={ item.id } onSubmit={ handleSubmit(() => state.addToCart(item.id)) }>
+            <li>
+              <p className="price">${ item.price }</p>
+              <span>{ item.title }</span>
+              <img src="./shopping-cart.png" alt="ilustrative item" />
+              <p> { item.description }</p>
+              <button type="submit">Add to cart</button>
+            </li>
+          </form>
         )
-      }
-      </ul>
-    </form>
+      )
+    }
+    </ul>
   )
 }
 
