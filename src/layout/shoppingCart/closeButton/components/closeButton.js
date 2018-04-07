334e60d4a1ee168e5835724a28cf5fc0883abed8
Gilmar Vanin <jvanin@thoughtworks.com>
2018-04-07 01:34:13 -0300

    Improves user UI for shopping cart.

--- src/layout/shoppingCart/closeButton/components/closeButton.js
+++ src/layout/shoppingCart/closeButton/components/closeButton.js
@@ -5,8 +5,8 @@ import './../closeButton.css'
 import CloseIcon from './../../../../resources/cancel.svg'
 
 const CloseButton = ({ toggleShoppingCart }) => (
-  <button className="closeShoppingCart" onClick={() => toggleShoppingCart()} type="submit">
-    <CloseIcon height="20px" width="20px" />
+  <button className="closeShoppingCartButton" onClick={() => toggleShoppingCart()} type="submit">
+    <CloseIcon className="closeIcon" height="20px" width="20px" />
   </button>
 )
 
--- src/layout/shoppingCart/components/shoppingCart.js
+++ src/layout/shoppingCart/components/shoppingCart.js
@@ -3,31 +3,34 @@ import { formatMoney } from 'accounting'
 import ProccedToCheckoutButton from './../proccedToCheckoutButton/components/proccedToCheckoutButton'
 import CloseButton from './../closeButton/containers/closeButton'
 
+import ShoppingCartIcon from './../../../resources/online-shopping.svg'
+
 const ShoppingCart = ({ items, displayShoppingCart }) => (
   <div className={displayShoppingCart ? 'shopping-cart displayShoppingCart' : 'shopping-cart hideShoppingCart'}>
     <CloseButton />
+    <ShoppingCartIcon className="shoppingCartIcon" width="70px" height="70px" />
+    <h3> MY SHOPPING CART ({ items.length })</h3>
     {
       items.length === 0
-      ? <p className="empty-shopping-cart">Your shopping cart is empty { ':(' } </p>
+      ? <p className="empty-shopping-cart">It is so lonely here { ':(' } </p>
       : buildShoppingCart(items)
     }
   </div>
 )
-
 const buildShoppingCart = items => (
   <React.Fragment>
     <ul>
       {
         items.map(item => (
           <li key={item.id}>
-            <p className="title"> { item.title } </p>
+            <p className="title"> <span> { '1x' } </span> { item.title } </p>
             <p className="price"> { formatMoney(item.price) } </p>
           </li>
         ))
       }
     </ul>
     <hr />
-    <p className="total">Total: { formatMoney(getTotal(items)) } </p>
+    <span className="total">Total:  { formatMoney(getTotal(items)) } </span>
     <ProccedToCheckoutButton />
   </React.Fragment>
 )
